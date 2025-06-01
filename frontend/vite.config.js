0689f220bc1f341be67f9e8d499dca28b0c46fc1
rahim2025 <mohammad.rahim.bhuiyan20@gmail.com>
2025-06-02 03:13:24 +0600

    Vercel.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,23 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5174,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  base: './',
 })
